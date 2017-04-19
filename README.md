37166d3fce42f15d65b333b061eefaf7630ede14
Han <hanyongtao@live.com>
2017-04-19 12:52:27 +0800

    add example

--- README.md
+++ README.md
@@ -51,5 +51,67 @@ timer = Timer.new() --创建timer对象
 
 -- timer:StopAllTimer()暂停所有计时器
 -- timer:StartAllTimer()暂停所有计时器
+```
+** timer with coroutine **
+```lua
+package.cpath = './?.dll;'
 
+require 'timer.c'
+require 'coroutine'
+require 'os'
+-- 一般一个lua state 只需要一个全局的 timer对象
+timer = Timer.new() --创建timer对象
+
+local function WaitSecond(sec)
+    return {Event='timer',dura = sec}
+end
+
+local function StartCoroutine(f)
+    local co = coroutine.create(f)
+
+    local schedule
+    schedule = function(corou)
+        local b,t = coroutine.resume(corou)
+        if b then
+            if t and t.Event and t.Event == 'timer' then
+                timer:ExpiredOnce(t.dura*1000,function()
+                    schedule(corou)
+                end)
+            end
+        end
+    end
+
+    schedule(co)
+end
+
+local function co3()
+    print("coroutine3 start",os.time())
+    coroutine.yield(WaitSecond(5))
+    print("coroutine3 5 second later",os.time())
+end
+
+StartCoroutine(function()
+    print("coroutine1 start",os.time())
+    coroutine.yield(WaitSecond(2))
+    print("coroutine1 2 second later",os.time())
+    StartCoroutine(co3)
+end)
+
+StartCoroutine(function()
+    print("coroutine2 start",os.time())
+    coroutine.yield(WaitSecond(1))
+    print("coroutine2 1 second later",os.time())
+    coroutine.yield(WaitSecond(1))
+    print("coroutine2 1 second later ",os.time())
+    coroutine.yield(WaitSecond(1))
+    print("coroutine2 1 second later ",os.time())
+    coroutine.yield(WaitSecond(1))
+    print("coroutine2 1 second later ",os.time() )
+end)
+
+print("main thread noblock")
+
+while true do
+    timer:Update()
+end
 ```
--- example/example_coroutine.lua
+++ example/example_coroutine.lua
@@ -2,6 +2,7 @@ package.cpath = './?.dll;'
 
 require 'timer.c'
 require 'coroutine'
+require 'os'
 -- 一般一个lua state 只需要一个全局的 timer对象
 timer = Timer.new() --创建timer对象
 
@@ -11,33 +12,45 @@ end
 
 local function StartCoroutine(f)
     local co = coroutine.create(f)
-    local b,t = coroutine.resume(co)
-    if b then
-        if t.Event == 'timer' then
-            timer:ExpiredOnce(t.dura*1000,function()
-                coroutine.resume(co)
-            end)
+
+    local schedule
+    schedule = function(corou)
+        local b,t = coroutine.resume(corou)
+        if b then
+            if t and t.Event and t.Event == 'timer' then
+                timer:ExpiredOnce(t.dura*1000,function()
+                    schedule(corou)
+                end)
+            end
         end
     end
+
+    schedule(co)
 end
 
 local function co3()
-    print("coroutine3 start")
+    print("coroutine3 start",os.time())
     coroutine.yield(WaitSecond(5))
-    print("coroutine3 5 second later")
+    print("coroutine3 5 second later",os.time())
 end
 
 StartCoroutine(function()
-    print("coroutine1 start")
+    print("coroutine1 start",os.time())
     coroutine.yield(WaitSecond(2))
-    print("coroutine1 2 second later")
+    print("coroutine1 2 second later",os.time())
     StartCoroutine(co3)
 end)
 
 StartCoroutine(function()
-    print("coroutine2 start")
+    print("coroutine2 start",os.time())
     coroutine.yield(WaitSecond(1))
-    print("coroutine2 1 second later")
+    print("coroutine2 1 second later",os.time())
+    coroutine.yield(WaitSecond(1))
+    print("coroutine2 1 second later ",os.time())
+    coroutine.yield(WaitSecond(1))
+    print("coroutine2 1 second later ",os.time())
+    coroutine.yield(WaitSecond(1))
+    print("coroutine2 1 second later ",os.time() )
 end)
 
 print("main thread noblock")
